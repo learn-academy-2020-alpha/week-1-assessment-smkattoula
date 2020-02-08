0f06d03dbe2104ecbe5cb5c757ee72a6e9f4fc50
smkattoula <60168409+smkattoula@users.noreply.github.com>
2020-02-07 16:24:31 -0800

    Add files via upload
    js-intro in class to be completed at home.

--- js-intro.js
+++ js-intro.js
@@ -7,9 +7,27 @@
 // --------------------1) Create a function that determines if a temperature is below boiling point, at boiling point or above boiling point. Boiling point is 212 degrees Farhenheit.
 // Use the test variables provided below. Expected outcome: "35 is below boiling point", "350 is above boiling point", "212 is at boiling point"
 
+
 var temp1 = 35
 var temp2 = 350
 var temp3 = 212
+
+// create a function called boilingPoint
+const boilingPoint = (number) => {
+// if number is less than 212 return 'number is below boiling point', if equals 12 return 'number is at boiling point', if number is above boiling point return 'number is above boiling point.' (if/else statement)'
+        if  (number < 212) {
+        return `${number} is below the boiling point.`
+    }
+    else if (number === 212){
+        return `${number} is at the boiling point`
+    }
+    else if (number > 212) {
+        return `${number} is above the boiling point`
+        }
+}
+console.log(boilingPoint(temp1));
+console.log(boilingPoint(temp2));
+console.log(boilingPoint(temp3));
 
 
 
@@ -18,7 +36,9 @@ var temp3 = 212
 // Use the test variable provided below. Expected outcome: [15, 35, 0, 30, -45]
 
 var myNumbers1 = [3, 7, 0, 6, -9]
-
+for (let i = 0; i < myNumbers1.length; i++) {
+    console.log(myNumbers1[i] * 5);
+}
 
 
 
@@ -26,6 +46,10 @@ var myNumbers1 = [3, 7, 0, 6, -9]
 // Use the test variable provided below. Expected outcome: [40, -35, 0, 30, 10]
 
 var myNumbers2 = [8, -7, 0, 6, 2]
+var newArr = myNumbers2.map(function(value) {
+    return value * 5
+})
+console.log(newArr)
 
 
 
@@ -36,9 +60,17 @@ var myNumbers2 = [8, -7, 0, 6, 2]
 var stringWithVowels1 = "HeyThereLearnStudent"
 var stringWithVowels2 = "ILoveJavaScript"
 
+removeVowel = (str) => {
+  let newArr1 = str.split("")
+  let newArr2 = newArr1.filter(value => value.toLowerCase() !== "a" && value.toLowerCase() !== "e" && value.toLowerCase() !== "i" && value.toLowerCase() !== "o" && value.toLowerCase() !== "u")
+  let newStr = newArr2.join("")
+  return newStr
+  }
 
 
 
+console.log(removeVowel(stringWithVowels1));
+console.log(removeVowel(stringWithVowels2));
 
 // --------------------5) Expand the logic from #4 to inform your user if the variable passed into the vowel removal function is not a string.
 // Copy and paste or rewrite the function. Use the test variables provided below.
